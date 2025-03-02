4b2758bf683062008c69d210166d7b32362af2e8
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-03-02 17:00:49 +0300

    4-й коммит, редактирование модели Post

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     """ Модель поста """
+
+    class Status(models.TextChoices):
+        """ Класс для управления статусом постов блога """
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250, verbose_name='Название поста', help_text='Напишите название')
     slug = models.SlugField(max_length=250)
     body = models.TextField(verbose_name='Содержание поста', help_text='Напишите описание')
     publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации поста')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания поста')
     updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления поста')
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT,
+                              verbose_name='Статус состояния поста (Черновик/Опубликован)')
 
     class Meta:
+        # Сортировка постов по дате публикации в порядке убывания
+        ordering = ['-publish']
+        # Индекс
+        indexes = [models.Index(fields=['-publish'])]
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
 
