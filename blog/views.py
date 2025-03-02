29b1c11b23630276698f05c0462f203a8e0ad587
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-03-02 20:15:22 +0300

    10-й коммит, вьюсеты

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    """ Выводит список постов """
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    """ Выводит детальную информацию о посте, при его отсутствие выдает ошибку код 404 (Не найдено) """
+    post = get_object_or_404(Post, pk=pk, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post_detail.html', {'post': post})
