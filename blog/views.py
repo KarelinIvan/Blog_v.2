a9761e92e825de9ef898e454aa8b85112c645d42
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-03-03 19:29:44 +0300

    14-й коммит, шаблоны

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,11 @@ from blog.models import Post
 
 def post_list(request):
     """ Выводит список постов """
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     """ Выводит детальную информацию о посте, при его отсутствие выдает ошибку код 404 (Не найдено) """
     post = get_object_or_404(Post, pk=pk, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
