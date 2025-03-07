bc38cff6c1c665be448228279ae6ee11c118a983
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-03-07 18:44:17 +0300

    19-й коммит, пагинация

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
@@ -5,7 +6,11 @@ from blog.models import Post
 
 def post_list(request):
     """ Выводит список постов """
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Постраничная разбивка по 3 поста на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
