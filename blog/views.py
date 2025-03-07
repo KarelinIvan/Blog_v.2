8bc130980cb520b70e1dbeff90d5cb2981848af3
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-03-07 20:02:14 +0300

    21-й коммит,

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
@@ -35,3 +36,18 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year,
                              publish__month=month, publish__day=day,)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    """ Извлекает пост по индентификатору id """
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
