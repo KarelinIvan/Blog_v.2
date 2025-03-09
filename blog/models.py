be3f43e6fb3d41abeb76c678a6af1737ca64f4cf
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-03-09 13:33:35 +0300

    27-й коммит, модель комментарии

--- blog/models.py
+++ blog/models.py
@@ -42,3 +42,20 @@ class Post(models.Model):
     def get_absolute_url(self):
         """ конвертирует логический адрес в физический URL-адрес целевых данных """
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug, ])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100, verbose_name='Автор комментария')
+    email = models.EmailField(verbose_name='e-mail')
+    body = models.TextField(verbose_name='Введите комментарий')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [models.Index(fields=['created'])]
+
+    def __str__(self):
+        return f'Комментарии {self.name} к {self.post}'
